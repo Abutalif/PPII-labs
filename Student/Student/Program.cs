574f444a321c921a4519bbe3aec887eaade9cc61
Abutalif <34387361+Abutalif@users.noreply.github.com>
2018-01-16 16:19:58 +0600

    Add files via upload

---
+++ Student/Student/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Student
+{
+    class Student
+    {
+        public string FirstName;
+        public string LastName;
+        public double GPA;
+
+        public void WriteFirstName()
+        {
+            FirstName = Console.ReadLine();   
+        }
+
+        public void WriteLastName()
+        {
+            LastName = Console.ReadLine();
+        }
+
+        public void WriteGPA()
+        {
+            GPA = int.Parse(Console.ReadLine());
+        }
+
+        public override string ToString()
+        {
+            return this.FirstName + " " + this.LastName + " " + this.GPA;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
