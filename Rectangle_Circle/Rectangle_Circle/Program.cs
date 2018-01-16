31d584ce899bafb7624b9df0292ab0bd8acd128c
Abutalif <34387361+Abutalif@users.noreply.github.com>
2018-01-16 16:37:27 +0600

    Add files via upload

---
+++ Rectangle_Circle/Rectangle_Circle/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rectangle_Circle
+{
+    class Circle
+    {
+        public double radius, area, diameter, circumference;
+        
+        public void getRadius()
+        {
+            radius = double.Parse(Console.ReadLine());
+        }
+
+        public void getDiameter ()
+        {
+            diameter = 2 * radius;
+        }
+
+        public void getArea()
+        {
+            area = radius * radius * Math.PI;
+        }
+
+        public void getCircumference()
+        {
+            circumference = diameter*Math.PI;
+        }
+    }
+
+
+    class Rectangle
+    {
+        double width, height, area, perimeter;
+
+        public void getWidth()
+        {
+            width = double.Parse(Console.ReadLine());
+        }
+
+        public void getHeight()
+        {
+            height = double.Parse(Console.ReadLine());
+        }
+
+        public void getArea()
+        {
+            area = height*width;
+        }
+
+        public void getPerimeter()
+        {
+            perimeter = 2 * (height + width);
+        }
+
+
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
