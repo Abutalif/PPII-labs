4deb31c4b2ac4475639948d7731adbd2e158db2f
Abutalif <34387361+Abutalif@users.noreply.github.com>
2018-01-28 22:30:52 +0600

    Add files via upload

---
+++ Task2-read_and_find/Task2-read_and_find/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task2_read_and_find
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamReader sr = new StreamReader(@"C:\Users\User\source\repos\Task2-read_and_find\Task2-read_and_find\nums.txt ");
+            
+            string[] arr = sr.ReadLine().Split(' ');
+            List<int> all = new List<int>();
+            int x;
+
+            /*foreach(string s in arr)
+            {
+                Console.Write(s + " ");
+            }*/
+
+           foreach (string s in arr)
+            {
+                x = int.Parse(s);
+                all.Add(x);
+            }
+
+            Console.WriteLine(all.Max() +" " + all.Min());
+
+
+            sr.Close();
+
+            Console.ReadKey();
+        }
+    }
+}
